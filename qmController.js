45061fecb323a0f913a5d0ddeb34a4514e6acd0b
grdesu <grdesu@gmail.com>
2015-02-16 20:02:11 +0600

    zxc zxc

---
+++ qmController.js
@@ -0,0 +1,64 @@
+var json = {
+	"text": "there's long way on the left side and a short one on the right",
+	"left": "left",
+	"right": "right",
+	"final": false,
+	"ways": [
+		{
+			"text": "there's worst way on the left and best one on the right",
+			"left": "worst",
+			"right": "best",
+			"final": false,
+			"ways": [
+				{
+					"text": "you lose",
+					"final": true
+				},
+				{
+					"text": "you're on the right way",
+					"left": "right",
+					"right": "left",
+					"final": false,
+					"ways": [
+						{
+							"text": "you win",
+							"final": true
+						},
+						{
+							"text": "you lose",
+							"final": true
+						}
+					]
+				}
+			]
+		},
+		{
+			"text": "you lose",
+			"final": true
+		}
+	]
+}
+
+
+function contentController($scope) {
+    $scope.json = json,
+    $scope.leftButton = function() {
+		$scope.json = $scope.json.ways[0];
+		if ($scope.json.final) {
+			$scope.hideControlButtons = true;
+			$scope.showRestartButton = true;
+		}
+	},
+	$scope.rightButton = function() {
+		$scope.json = $scope.json.ways[1];
+		if ($scope.json.final) {
+			$scope.hideControlButtons = true;
+			$scope.showRestartButton = true;
+		}
+	},
+	$scope.restart = function() {
+		$scope.showRestartButton = false;
+		$scope.json = json;
+		$scope.hideControlButtons = false;
+	}
+}
