dcdff886cd85dc6dafffa21da63ddd97e131835f
grdesu <grdesu@gmail.com>
2015-02-17 15:24:01 +0600

    v0.0.0.0.0.0.2

--- qmController.js
+++ qmController.js
@@ -1,27 +1,55 @@
 var quest = {
+	"title": "SUPER QUEST",
 	"text": "there's long way on the left side and a short one on the right",
-	"left": "left",
-	"right": "right",
+	"first": "left",
+	"second": "right",
 	"final": false,
 	"ways": [
 		{
 			"text": "there's worst way on the left and best one on the right",
-			"left": "worst",
-			"right": "best",
+			"first": "worst",
+			"second": "best",
 			"final": false,
 			"ways": [
 				{
-					"text": "you lose",
-					"final": true
+					"text": "you lose, I have no idea why you should chose something",
+					"final": false,
+					"first": "time",
+					"second": "year",
+					"third": "people",
+					"fourth": "way",
+					"ways": [
+						{
+							"text": "you lose",
+							"final": "true"
+						},
+						{
+							"text": "you lose",
+							"final": "true"
+						},
+						{
+							"text": "you lose",
+							"final": "true"						
+						},
+						{
+							"text": "you lose",
+							"final": "true"		
+						}
+					]
 				},
 				{
 					"text": "you're on the right way",
-					"left": "right",
-					"right": "left",
+					"first": "right",
+					"second": "wait",
+					"third": "left",
 					"final": false,
 					"ways": [
 						{
 							"text": "you win",
+							"final": true
+						},
+						{
+							"text": "you have to think faster, loser",
 							"final": true
 						},
 						{
@@ -40,25 +68,43 @@ var quest = {
 }
 
 
-function contentController($scope) {
+function hideSomeButtons($scope) {
+	$scope.hideSecondButton = false;
+	$scope.hideThirdButton = false;	
+	$scope.hideFourthButton = true;
+	if ($scope.json.ways.length == 1) {
+		$scope.hideSecondButton = true;
+		$scope.hideThirdButton = true;
+	}
+	if ($scope.json.ways.length == 2) {
+		$scope.hideThirdButton = true;			
+	}
+	if ($scope.json.ways.length == 4) {
+		$scope.hideFourthButton = false;			
+	}
+}
+
+angular.module("ngApp", [])
+.controller("contentController", function($scope) {
     $scope.json = quest,
-    $scope.leftButton = function() {
-		$scope.json = $scope.json.ways[0];
+	$scope.buttonClick = function(i) {
+		$scope.json = $scope.json.ways[i];
 		if ($scope.json.final) {
-			$scope.hideControlButtons = true;
+			$scope.showControlButtons = false;
 			$scope.showRestartButton = true;
 		}
-	},
-	$scope.rightButton = function() {
-		$scope.json = $scope.json.ways[1];
-		if ($scope.json.final) {
-			$scope.hideControlButtons = true;
-			$scope.showRestartButton = true;
-		}
+		hideSomeButtons($scope);
 	},
 	$scope.restart = function() {
 		$scope.showRestartButton = false;
 		$scope.json = quest;
-		$scope.hideControlButtons = false;
+		$scope.showControlButtons = true;
+		hideSomeButtons($scope);
+	},
+	$scope.start = function() {
+		$scope.hideStart = true;
+		$scope.showControlButtons = true;
+		$scope.showText = true;
+		hideSomeButtons($scope);
 	}
-}
+});
