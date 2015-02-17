db5ca02a14ffb485d7ca32f845edc98f7158001b
grdesu <grdesu@gmail.com>
2015-02-17 20:12:24 +0600

    v0.0.0.0.0.0.3-1

--- qmController.js
+++ qmController.js
@@ -1,26 +1,12 @@
 var quest = {}
 
-function hideSomeButtons($scope) {
-	$scope.hideSecondButton = false;
-	$scope.hideThirdButton = true;
-	$scope.hideFourthButton = true;
-	if ($scope.json.ways.length == 1) {
-		$scope.hideSecondButton = true;
-	}
-	if ($scope.json.ways.length == 3) {
-		$scope.hideThirdButton = false;			
-	}
-	if ($scope.json.ways.length == 4) {
-		$scope.hideThirdButton = false;			
-		$scope.hideFourthButton = false;			
-	}
-}
 
 function downloadQuest($scope, $http, currentQuest, url) {
 	$http.get(url).
 				success(function(data, status, headers, config) {
 					$scope.json = data;
 					currentQuest = $scope.json;
+					$scope.chooseButtons();
 				}).
 				error(function(data, status, headers, config) {
 					$scope.startError = true;
@@ -31,34 +17,54 @@ var currentQuest;
 
 angular.module("ngApp", [])
 .controller("contentController", function($scope, $http) {
-	$scope.str_quest = '{"text":"this is example quest","first":"first","second":"second","final":false,"ways":[{"text":"you win","final":true},{"text":"you lose","final":true}]}';
+	$scope.chooseButtons = function() {
+		if (!$scope.json.final) {
+			$scope.hideSecondButton = false;
+			$scope.hideThirdButton = true;
+			$scope.hideFourthButton = true;
+			if ($scope.json.ways.length == 1) {
+				$scope.hideSecondButton = true;
+				$scope.hideThirdButton = true;
+				$scope.hideFourthButton = true;
+			}
+			if ($scope.json.ways.length == 3) {
+				$scope.hideThirdButton = false;
+			}
+			if ($scope.json.ways.length == 4) {
+				$scope.hideThirdButton = false;
+				$scope.hideFourthButton = false;
+			}
+		}
+	};
+	//$scope.chooseButtons();
+	$scope.str_quest = '{"text":"this is example quest","first":"first","second":"second","third":"third","final":false,"ways":[{"text":"you win","final":true},{"text":"you lose","final":true},{"text":"you lose","final":true}]}';
 	$scope.buttonClick = function(i) {
-		$scope.json = $scope.json.ways[i];
-		if ($scope.json.final) {
-			$scope.showControlButtons = false;
-			$scope.showRestartButton = true;
+		if (i != null) {
+			$scope.json = $scope.json.ways[i];
+			$scope.chooseButtons();
+			if ($scope.json.final) {
+				$scope.showControlButtons = false;
+				$scope.showRestartButton = true;
+			}
 		}
 	};
 	$scope.restart = function() {
 		$scope.showRestartButton = false;
-		$scope.json = currentQuest;
 		$scope.hideStart = false;
 		$scope.showText = false;
-		hideSomeButtons($scope);
 	};
 	$scope.start = function(i) {
-		downloadQuest($scope, $http, currentQuest, "http://grdesu.github.io/quest" + i.toString() + ".json");
+		downloadQuest($scope, $http, currentQuest, "/quest" + i.toString() + ".json");
 		$scope.hideStart = true;
 		$scope.showControlButtons = true;
 		$scope.showText = true;
-		hideSomeButtons($scope);
+		showControlButtons($scope);
 	};
 	$scope.custom = function() {
-		//downloadQuest($scope, $http, currentQuest, $scope.q_url);
 		$scope.json = angular.fromJson($scope.str_quest);
 		$scope.hideStart = true;
 		$scope.showControlButtons = true;
 		$scope.showText = true;
-		hideSomeButtons($scope);
+		$scope.chooseButtons();
 	}
 });
