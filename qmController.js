af898762a4c48d949d2c3308566b2ed29e54666e
grdesu <grdesu@gmail.com>
2015-02-23 19:45:19 +0600

    Quest maker upgraded
    Now u can create multi-way quests

--- qmController.js
+++ qmController.js
@@ -6,13 +6,35 @@ function isLocalStorageAvailable() {
   }
 }
 
+
 angular.module("ngApp", [])
 .controller("creatorController", function($scope, $http) {
+	$scope.quest = {"title":"generated quest","nodes":[]};
+	$scope.newNodeId = 0;
+	$scope.selectedNodeId = 0;
+	$scope.add = function() {
+		node = {"id":$scope.newNodeId, "text": $scope.user_text};
+		var ways_ids = [];
+		var ways = [];
+
+		for (i = 0; i < 4; ++i) {
+			if ($scope.buttons[i] !== null && $scope.buttons_dest[i] !== null) {
+				ways_ids.push(parseInt($scope.buttons_dest[i]));
+				ways.push($scope.buttons[i]);
+			}
+		}
+
+		if ($scope.isNodeFinal)
+			node.final = true;
+		node.ways_ids = ways_ids;
+		node.ways = ways;
+		
+		$scope.quest.nodes.push(node);
+		++$scope.newNodeId;
+		$scope.selectedNodeId = $scope.newNodeId;
+	}
 	$scope.generate = function() {
-		$scope.result = '{"title": "generated quest","nodes":[{"id": 0,"text":"' + $scope.user_text + '","ways_ids":\
-[1, 2],"ways": ["' + $scope.user_first + '","' + $scope.user_second + '"]},\
-{"id": 1,"text":"you win","final": true},{"id": 2,"text": "you lose","final": true}]}'
-		;
+		$scope.result = angular.toJson($scope.quest);		
 		if (isLocalStorageAvailable()) {
 			sessionStorage.setItem('quest', $scope.result);	
 		}
